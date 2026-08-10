3068576dccccfccd4d1bec36ced1a7ffc4486e93
Deepika Tyagi <deepika.tyagi2005@gmail.com>
2026-08-10 14:05:28 +0530

    Time: 4 ms (80.09%) | Memory: 46.8 MB (64.37%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -9,36 +9,24 @@
  * }
  */
 class Solution {
-    static ListNode merge2lists(ListNode h1,ListNode h2){
-        ListNode t1=h1;
-        ListNode t2=h2;
-        ListNode dummy = new ListNode(0);
-        ListNode temp=dummy;
-        while(t1!=null && t2 != null){
-            if(t1.val < t2.val){
-                temp.next=t1;
-                t1=t1.next;
-                temp=temp.next;
-            }else{
-                temp.next=t2;
-                t2=t2.next;
-                temp=temp.next;
+    public ListNode mergeKLists(ListNode[] lists){
+        PriorityQueue<ListNode> pq=new PriorityQueue<>((a,b)-> a.val-b.val);
+        for(ListNode list : lists){
+            if(list != null){
+                pq.add((list));
             }
         }
-        if(t1!=null){
-            temp.next=t1;
-        }else{
-            temp.next=t2;
+
+        ListNode dummy = new ListNode(0);
+        ListNode temp=dummy;
+        while(!pq.isEmpty()){
+            ListNode smallest = pq.poll();
+            temp.next=smallest;
+            temp=temp.next;
+            if(smallest.next != null){
+                pq.add(smallest.next);
+            }
         }
         return dummy.next;
-    }
-    public ListNode mergeKLists(ListNode[] lists){
-     
-    if(lists.length==0){return null;}
-    ListNode head = lists[0];
-    for(int i = 1;i<lists.length;i++){
-        head = merge2lists(head,lists[i]);
-    }
-        return head;
     }
 }
