9e49fd2cdfc54c256d6b8a71f6eabe4aaa616208
Deepika Tyagi <deepika.tyagi2005@gmail.com>
2026-08-10 13:26:53 +0530

    Time: 87 ms (6.12%) | Memory: 46.8 MB (46.41%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -9,25 +9,36 @@
  * }
  */
 class Solution {
-    public ListNode mergeKLists(ListNode[] lists) {
-
-        List<Integer> l = new ArrayList<>();
-        for(int i = 0;i<lists.length;i++){
-            ListNode temp = lists[i];
-            while(temp != null){
-                l.add(temp.val);
+    static ListNode merge2lists(ListNode h1,ListNode h2){
+        ListNode t1=h1;
+        ListNode t2=h2;
+        ListNode dummy = new ListNode(0);
+        ListNode temp=dummy;
+        while(t1!=null && t2 != null){
+            if(t1.val < t2.val){
+                temp.next=t1;
+                t1=t1.next;
+                temp=temp.next;
+            }else{
+                temp.next=t2;
+                t2=t2.next;
                 temp=temp.next;
             }
         }
-
-        Collections.sort(l);
-        ListNode dummy = new ListNode(0);
-        ListNode temp=dummy;
-        for(int i = 0;i<l.size();i++){
-            temp.next= new ListNode(l.get(i));
-            temp=temp.next;
+        if(t1!=null){
+            temp.next=t1;
+        }else{
+            temp.next=t2;
         }
         return dummy.next;
-        
+    }
+    public ListNode mergeKLists(ListNode[] lists){
+     
+    if(lists.length==0){return null;}
+    ListNode head = lists[0];
+    for(int i = 1;i<lists.length;i++){
+        head = merge2lists(head,lists[i]);
+    }
+        return head;
     }
 }
