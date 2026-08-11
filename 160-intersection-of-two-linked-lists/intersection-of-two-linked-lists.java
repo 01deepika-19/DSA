5742a4c428903566a6fe21df11ad2ed1f745ed6c
Deepika Tyagi <deepika.tyagi2005@gmail.com>
2026-08-11 17:12:09 +0530

    Time: 1 ms (99.91%) | Memory: 52.7 MB (64.02%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -10,39 +10,19 @@
  * }
  */
 public class Solution {
-
-
-    static ListNode collisionPoint(ListNode h1,ListNode h2,int diff){
-
-        ListNode t1=h1;
-        ListNode t2=h2;
-        for(int i = 0;i<diff;i++){
-            t2=t2.next;
-        }
-        while(t1 != t2){
-            t1=t1.next;
-            t2=t2.next;
-        }
-        return t1;
-    }
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+        if(headA == null || headB == null){return null;}
 
         ListNode t1=headA;
         ListNode t2=headB;
-        int n1=0,n2=0;
-        while(t1 != null){
-            n1++;
+        while(t1 != t2){
             t1=t1.next;
-        }
-        while(t2!=null){
-            n2++;
             t2=t2.next;
+            if(t1==t2){return t1;}
+            if(t1==null){t1=headB;}
+            if(t2==null){t2=headA;}
         }
-        if(n1 < n2){
-            return collisionPoint(headA,headB,n2-n1);
-        }else{
-            return collisionPoint(headB,headA,n1-n2);
-        }
-        
+       return t1;
     }
 }
