580277eeae21f802dba6ffa86baeeb857c74a2d0
Deepika Tyagi <deepika.tyagi2005@gmail.com>
2026-08-26 13:00:02 +0530

    Time: 8 ms (74.46%) | Memory: 65.3 MB (40.83%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,31 +1,36 @@
 class Solution {
     public List<List<String>> partition(String s) {
-        List<List<String>> ans = new ArrayList<>();
-        generate(0,s,new ArrayList<>(),ans);
-        return ans; 
+       List<List<String>> ans = new ArrayList<>();
+       generate(0,s,new ArrayList<>(),ans);
+       return ans;
     }
 
-    private void generate(int index,String s,List<String> current,List<List<String>> ans){
+    private void generate(int index,String s,List<String> list,List<List<String>>ans){
         if(index == s.length()){
-            ans.add(new ArrayList<>(current));
+            ans.add(new ArrayList<>(list));
             return;
         }
+
         for(int i = index;i<s.length();i++){
             if(isPalindrome(s,index,i)){
-                current.add(s.substring(index,i+1));
-                generate(i+1,s,current,ans);
-                current.remove(current.size()-1);
+
+                list.add(s.substring(index,i+1));
+                generate(i+1,s,list,ans);
+                list.remove(list.size()-1);
             }
         }
-    }
-    private boolean isPalindrome(String s,int left,int right){
-        while(left <right){
-            if(s.charAt(left)!=s.charAt(right)){
+        }
+
+    private boolean isPalindrome(String s,int start,int end){
+
+        while(start<=end){
+            if(s.charAt(start) != s.charAt(end)){
                 return false;
             }
-            left++;
-            right--;
+            start++;
+            end--;
         }
         return true;
     }
-}
+
+    }
