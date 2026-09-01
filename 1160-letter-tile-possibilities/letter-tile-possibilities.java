1d24aefe2c03d6a02abcc5b45ea4f39e92756893
Deepika Tyagi <deepika.tyagi2005@gmail.com>
2026-09-01 11:58:27 +0530

    Time: 1 ms (97.43%) | Memory: 42.9 MB (50.05%) - LeetSync

---
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numTilePossibilities(String tiles) {
+        char arr[]= tiles.toCharArray();
+        Arrays.sort(arr);
+        boolean used[]=new boolean[arr.length];
+        return solve(arr,used);
+    }
+
+    private int solve(char arr[],boolean used[]){
+        int count =0;
+        for(int i = 0;i<arr.length;i++){
+            if(used[i]){
+                continue;
+            }
+            if(i>0 && arr[i]==arr[i-1] && !used[i-1]){
+                continue;
+            }
+                used[i]=true;
+                count += 1 + solve(arr,used);
+                used[i]=false;
+            }
+        return count;
+        }
+}
