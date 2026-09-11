28bd35b6812dc81c9c65748cb1c0cd5bb1ef5c1e
Deepika Tyagi <deepika.tyagi2005@gmail.com>
2026-09-11 12:12:03 +0530

    Time: 916 ms (13.14%) | Memory: 50.6 MB (9.47%) - LeetSync

--- 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.java
+++ 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.java
@@ -1,18 +1,21 @@
 class Solution {
     public int maxProduct(String[] words) {
-
-        int mask[]=new int[words.length];
-        for(int i = 0;i<words.length;i++){
-            for(char c : words[i].toCharArray()){
-                mask[i] |= (1<<(c-'a')); 
-            }
-        }
-        
         int ans = 0;
         for(int i = 0;i<words.length;i++){
+            boolean present[] = new boolean[26];
+            for(char c : words[i].toCharArray()){
+                present[c-'a']=true;
+            }
             for(int j = i+1;j<words.length;j++){
-                if((mask[i] & mask[j]) == 0){
-                    ans = Math.max(ans,words[i].length()*words[j].length());
+                boolean common = false;
+                for(char ch : words[j].toCharArray()){
+                    if(present[ch-'a']){
+                        common = true;
+                        break;
+                    }
+                }
+                if(!common){
+                ans = Math.max(ans,words[i].length()*words[j].length());
                 }
             }
         }
